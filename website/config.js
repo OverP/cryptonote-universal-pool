357bcec2f4fadb1f194eb8ad08eef9a3350eee1e
OverP <overplayeryt@gmail.com>
2017-09-06 14:05:53 +0200

    updated for test pool

--- website/config.js
+++ website/config.js
@@ -1,17 +1,17 @@
-var api = "http://poolhost.com:1117";
+var api = "http://m0n3r0p00lt3st.gq:1117";
 
-var poolHost = "poolhost.com";
+var poolHost = "m0n3r0p00lt3st.gq";
 
 var irc = "irc.freenode.net/#poolhost";
 
-var email = "support@poolhost.com";
+var email = "support@m0n3r0p00lt3st.gq";
 
-var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+var cryptonatorWidget = ["{XMR}-BTC", "{XMR}-USD", "{XMR}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "http://chainradar.com/{XMR}/block/{id}";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "http://chainradar.com/{XMR}/transaction/{id}";
 
 var themeCss = "themes/default-theme.css";
